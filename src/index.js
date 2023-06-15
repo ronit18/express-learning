e44c208dc46b9f4b3a10460badb65391b86188c4
ronit18 <ronitgandhi96@gmail.com>
2023-06-15 22:45:08 +0530

    Learning about get and post request.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
 const PORT = 3001;
+
+const items = [
+	{
+		item: 'new',
+		total: 43,
+	},
+
+	{
+		item: 'new',
+		total: 43,
+	},
+];
+
+app.get('/', (req, res) => {
+	res.send(items);
+});
+app.post('/post', (req, res) => {
+	console.log(req.body);
+	items.push(req.body);
+	res.sendStatus(200);
+});
 
 app.listen(PORT, () => console.log(`Running server on ${PORT} 🚀 `));
