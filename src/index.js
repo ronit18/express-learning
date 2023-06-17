276c09ed4ab25e27abaf77b89d9da7a0735f4f22
ronit18 <ronitgandhi96@gmail.com>
2023-06-17 15:29:27 +0530

    Learned about middleware.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ app.use(express.json());
 app.use(express.urlencoded());
 const PORT = 3001;
 
+app.use((req, res, next) => {
+	console.log(`Request ${req.method} ${req.url} ${new Date()}`);
+	next();
+});
+
 const items = [
 	{
 		item: 'new',
@@ -17,9 +22,10 @@ const items = [
 	},
 ];
 
-app.get('/', (req, res) => {
+app.get('/list', (req, res) => {
 	res.send(items);
 });
+
 app.post('/post', (req, res) => {
 	console.log(req.body);
 	items.push(req.body);
